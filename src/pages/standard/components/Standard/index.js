9e6810f19b62fa7c78e720f06b3cb69233cc7f6c
wangxgk <wangxgk@yonyou.com>
2019-01-07 16:05:10 +0800

    添加standard节点

---
+++ src/pages/standard/components/Standard/index.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import {actions} from 'mirrorx';
+import {Button} from 'tinper-bee';
+
+import './index.less';
+
+class Standard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    handleClick = async () => {
+    }
+
+    componentDidMount() {
+        
+    }
+
+    render() {
+
+        return (
+            <div >
+               Standard
+            </div>
+        )
+    }
+}
+
+export default Standard;
